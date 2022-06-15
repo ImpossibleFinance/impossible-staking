d3794794cf60d34bfd237f9dbd29029466ad7fd2
Accelerated Monkey <acceleratedape@protonmail.com>
2022-06-15 20:11:30 +0700

    update contract to be more generic, change blocktime to timestamp

--- contracts/ImpossibleStaking.sol
+++ contracts/ImpossibleStaking.sol
@@ -18,14 +18,14 @@ contract ImpossibleStaking is Ownable {
     struct PoolInfo {
         IERC20 lpToken;           // Address of LP token contract.
         uint256 allocPoint;       // How many allocation points assigned to this pool.
-        uint256 lastRewardBlock;  // Last block number that IF distribution occurs.
-        uint256 accIFPerShare; // Accumulated IF per share, times 1e12. See below.
+        uint256 lastRewardTimestamp;  // Last timestamp number that IF distribution occurs.
+        uint256 accRewardPerShare; // Accumulated IF per share, times 1e12. See below.
     }
 
-    IERC20 public ifToken;
-    uint256 public startBlock;
-    uint256 public endBlock;
-    uint256 public ifPerBlock;
+    IERC20 public rewardToken;
+    uint256 public startTimestamp;
+    uint256 public endTimestamp;
+    uint256 public rewardPerTimestamp;
     uint256 public totalAllocPoint = 0;
 
     PoolInfo[] public poolInfo;
@@ -38,14 +38,14 @@ contract ImpossibleStaking is Ownable {
 
     constructor(
         IERC20 _if,
-        uint256 _ifPerBlock,
-        uint256 _startBlock,
-        uint256 _endBlock
+        uint256 _rewardPerTimestamp,
+        uint256 _startTimestamp,
+        uint256 _endTimestamp
     ) public {
-        ifToken = _if;
-        ifPerBlock = _ifPerBlock;
-        endBlock = _endBlock;
-        startBlock = _startBlock;
+        rewardToken = _if;
+        rewardPerTimestamp = _rewardPerTimestamp;
+        endTimestamp = _endTimestamp;
+        startTimestamp = _startTimestamp;
     }
 
     function poolLength() external view returns (uint256) {
@@ -56,13 +56,13 @@ contract ImpossibleStaking is Ownable {
     // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
     function add(uint256 _allocPoint, IERC20 _lpToken) public onlyOwner {
         massUpdatePools();
-        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        uint256 lastRewardTimestamp = block.timestamp > startTimestamp ? block.timestamp : startTimestamp;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
         poolInfo.push(PoolInfo({
             lpToken: _lpToken,
             allocPoint: _allocPoint,
-            lastRewardBlock: lastRewardBlock,
-            accIFPerShare: 0
+            lastRewardTimestamp: lastRewardTimestamp,
+            accRewardPerShare: 0
         }));
     }
 
@@ -73,25 +73,25 @@ contract ImpossibleStaking is Ownable {
     }
 
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
-        uint _endBlock = endBlock; // gas savings
-        if (_from >= _endBlock) {
+        uint _endTimestamp = endTimestamp; // gas savings
+        if (_from >= _endTimestamp) {
             return 0;
         } else {
-            return _to >= _endBlock ? _endBlock.sub(_from) : _to.sub(_from);
+            return _to >= _endTimestamp ? _endTimestamp.sub(_from) : _to.sub(_from);
         }
     }
 
     function pendingIF(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accIFPerShare = pool.accIFPerShare;
+        uint256 accRewardPerShare = pool.accRewardPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 ifReward = multiplier.mul(ifPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accIFPerShare = accIFPerShare.add(ifReward.mul(1e12).div(lpSupply));
+        if (block.timestamp > pool.lastRewardTimestamp && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardTimestamp, block.timestamp);
+            uint256 ifReward = multiplier.mul(rewardPerTimestamp).mul(pool.allocPoint).div(totalAllocPoint);
+            accRewardPerShare = accRewardPerShare.add(ifReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accIFPerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accRewardPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -104,18 +104,18 @@ contract ImpossibleStaking is Ownable {
 
     function updatePool(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
-        if (block.number <= pool.lastRewardBlock) {
+        if (block.timestamp <= pool.lastRewardTimestamp) {
             return;
         }
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (lpSupply == 0) {
-            pool.lastRewardBlock = block.number;
+            pool.lastRewardTimestamp = block.timestamp;
             return;
         }
-        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 ifReward = multiplier.mul(ifPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        pool.accIFPerShare = pool.accIFPerShare.add(ifReward.mul(1e12).div(lpSupply));
-        pool.lastRewardBlock = block.number;
+        uint256 multiplier = getMultiplier(pool.lastRewardTimestamp, block.timestamp);
+        uint256 ifReward = multiplier.mul(rewardPerTimestamp).mul(pool.allocPoint).div(totalAllocPoint);
+        pool.accRewardPerShare = pool.accRewardPerShare.add(ifReward.mul(1e12).div(lpSupply));
+        pool.lastRewardTimestamp = block.timestamp;
     }
 
     // Deposit LP tokens to ImpossibleStaking contract for IF allocation.
@@ -124,7 +124,7 @@ contract ImpossibleStaking is Ownable {
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accIFPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accRewardPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
                 safeIFTransfer(msg.sender, pending);
             }
@@ -133,7 +133,7 @@ contract ImpossibleStaking is Ownable {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accIFPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accRewardPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -143,7 +143,7 @@ contract ImpossibleStaking is Ownable {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accIFPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accRewardPerShare).div(1e12).sub(user.rewardDebt);
         if(pending > 0) {
             safeIFTransfer(msg.sender, pending);
         }
@@ -151,7 +151,7 @@ contract ImpossibleStaking is Ownable {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accIFPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accRewardPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -167,19 +167,19 @@ contract ImpossibleStaking is Ownable {
 
     // Safe if transfer function, just in case if rounding error causes pool to not have enough IFs.
     function safeIFTransfer(address _to, uint256 _amount) internal {
-        uint256 ifBal = ifToken.balanceOf(address(this));
+        uint256 ifBal = rewardToken.balanceOf(address(this));
         if (_amount > ifBal) {
-            ifToken.transfer(_to, ifBal);
+            rewardToken.transfer(_to, ifBal);
         } else {
-            ifToken.transfer(_to, _amount);
+            rewardToken.transfer(_to, _amount);
         }
     }
 
-    // Withdraws remaining IF balance in contract. Can only be called after endblock
+    // Withdraws remaining IF balance in contract. Can only be called after endTimestamp
     function removeIFBal(uint amount) external onlyOwner {
-        require(block.number > endBlock, "only can withdraw IF after endBlock");
+        require(block.timestamp > endTimestamp, "only can withdraw IF after endTimestamp");
         safeIFTransfer(_msgSender(), amount);
-        emit DevWithdraw(address(ifToken), amount);
+        emit DevWithdraw(address(rewardToken), amount);
     }
 
     // retrieve other tokens erroneously sent in to this address
